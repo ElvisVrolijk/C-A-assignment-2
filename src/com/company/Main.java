4ce242791db3edf8489022e7102895ef20381fdf
Elvis <e_voetbal_guy@hotmail.com>
2018-01-07 14:53:22 +0100

    add delete and update methode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    private DEPQ depq;
+    private Minmaxheap minmaxheap;
 
     public static void main(String[] args) {
         // write your code here
@@ -13,20 +14,7 @@ public class Main {
     }
 
     public void test() {
-//        for (int i = 0; i < 10; i++) {
-//            int level = (int) (Math.log(i + 1) / Math.log(2)) % 2;
-////            System.out.println("log (" + i + ") / log(2) = " + level);
-//            if (i > 0) {
-//                int calPar = i / 2 + 1;
-//                int parentNode = i - calPar;
-//                int calGrand = parentNode / 2 + 1;
-//                int grandParent = parentNode - calGrand;
-//
-//                System.out.println("parent node of (" + i + "): " + parentNode);
-//                System.out.println("grandparent node: " + grandParent);
-//            }
-//
-//        }
+
         List<Integer> list = new ArrayList<>();
         list.add(18);
         list.add(5);
@@ -34,40 +22,35 @@ public class Main {
         list.add(20);
         list.add(45);
         list.add(33);
-        list.add(4);
+        list.add(8);
         list.add(11);
         list.add(1);
+        list.add(42);
+        list.add(30);
 
         for (Integer integer : list) {
             System.out.print(integer + " ");
         }
-        depq = new DEPQ(list);
-        depq.prioritize();
+
+        minmaxheap = new Minmaxheap(list);
+        minmaxheap.prioritize();
 
         System.out.println(" ");
 
-//        int toPrint = 1;
-//        int nodeInLine = 1;
-//        int i = 0;
-//        String space = " ";
-//        while (i < depq.getQueueArray().size()) {
-//            toPrint--;
-//
-//            System.out.print(depq.getQueueArray().get(i));
-//            i++;
-//            if (toPrint == 0) {
-//                nodeInLine *= 2;
-//                toPrint = nodeInLine;
-//                System.out.println();
-//            }
-//        }
+        for (Integer integer : minmaxheap.getQueueArray()) {
+            System.out.print(integer + " ");
+        }
 
-        for(int i=0;i<depq.getQueueArray().size();i++){
-            for(int j=0;j<Math.pow(2,i)&&j+Math.pow(2,i)<10;j++){
-                System.out.print(depq.getQueueArray().get(j+(int)Math.pow(2,i)-1)+" ");
+        minmaxheap.delete(1);
+        minmaxheap.delete(5);
+        minmaxheap.delete(45);
+        minmaxheap.delete(18);
 
-            }
-            System.out.println();
+        System.out.println(" ");
+
+        for (Integer integer : minmaxheap.getQueueArray()) {
+            System.out.print(integer + " ");
         }
     }
+
 }
---
+++ src/com/company/Minmaxheap.java
@@ -0,0 +1,524 @@
+package com.company;
+
+import sun.plugin2.os.windows.FLASHWINFO;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.swap;
+
+public class Minmaxheap {
+    private List<Integer> queueArray;
+
+    // Node
+    private int nodePosition;
+    private int node;
+
+    // Parent
+    private int parentPosition;
+    private int parent;
+
+    // Grand parent
+    private int grandParentPosition;
+    private int grandParent;
+
+    // Children
+    private HashMap<Integer, Integer> children = new HashMap<>();
+
+    // Child
+    private int childPosition;
+    private int child;
+
+    // Grand Children
+    private HashMap<Integer, Integer> grandChildren = new HashMap<>();
+
+    // Grand child
+    private int grandChildPosition;
+    private int grandChild;
+
+    private boolean hasParent = false;
+    private boolean hasGrandParent = false;
+    private boolean hasChild = false;
+    private boolean hasGrandChild = false;
+
+
+    private boolean swapped = false;
+
+    public Minmaxheap(List<Integer> queueArray) {
+        this.queueArray = queueArray;
+    }
+
+    public boolean isEmpty() {
+        return queueArray.size() < 1;
+    }
+
+    public int size() {
+        return queueArray.size();
+    }
+
+    public int getMin() {
+        return queueArray.get(queueArray.size() - 1);
+    }
+
+    public int getMax() {
+        return queueArray.get(0);
+    }
+
+    public void insert(int x) {
+        queueArray.add(x);
+        prioritize();
+    }
+
+    public void removeMin() {
+        queueArray.remove(queueArray.size() - 1);
+    }
+
+    public void removeMax() {
+        queueArray.remove(0);
+    }
+
+    public List<Integer> getQueueArray() {
+        return queueArray;
+    }
+
+    private int getLevel(int i) {
+        return (int) (Math.log(i + 1) / Math.log(2)) % 2;
+    }
+
+    /**
+     * @param nodePosition is used to calculate parent position
+     * @return parent position
+     */
+    private int parentPosition(int nodePosition) {
+        return nodePosition - (nodePosition / 2 + 1);
+    }
+
+    /**
+     * @param parentPosition is used to get parent
+     * @return parent
+     */
+    private int getParent(int parentPosition) {
+        return queueArray.get(parentPosition);
+    }
+
+    /**
+     * @param nodePosition is used to calculate grand parent position
+     * @return grand parent position
+     */
+    private int grandParentPosition(int nodePosition) {
+        int parentPosition = parentPosition(nodePosition);
+        return parentPosition - (parentPosition / 2 + 1);
+    }
+
+    /**
+     * @param grandParentPosition is used to get grand parent
+     * @return grand parent
+     */
+    private int getGrandParent(int grandParentPosition) {
+        return queueArray.get(grandParentPosition);
+    }
+
+
+
+    private void addChildToMap(int childPosition) {
+        if (childPosition <= queueArray.size() - 1) {
+            int child = queueArray.get(childPosition);
+            children.put(childPosition, child);
+        }
+    }
+
+    private void addGrandChildToMap(int grandChildPosition) {
+        if (grandChildPosition <= queueArray.size() - 1) {
+            int grandChild = queueArray.get(grandChildPosition);
+            grandChildren.put(grandChildPosition, grandChild);
+        }
+    }
+
+    private void lookForChildren(int nodePosition) {
+        children.clear();
+
+        int child1Position = nodePosition * 2 + 1;
+        int child2Position = nodePosition * 2 + 2;
+
+        addChildToMap(child1Position);
+
+        addChildToMap(child2Position);
+    }
+
+    private int minChildPosition() {
+        int minPosition = -10;
+        for (Map.Entry map : children.entrySet()) {
+            int currentPosition = (int) map.getKey();
+            int currentValue = (int) map.getValue();
+            if (minPosition == -10 || currentValue < queueArray.get(minPosition)) {
+                minPosition = currentPosition;
+            }
+        }
+
+        return minPosition;
+    }
+
+    private void findSmallestChild() {
+        lookForChildren(nodePosition);
+
+        if (children.size() > 0) {
+            childPosition = minChildPosition();
+            child = children.get(childPosition);
+            hasChild = true;
+        } else {
+            hasChild = false;
+        }
+    }
+
+    private int maxChildPosition() {
+        int maxPosition = -10;
+        for (Map.Entry map : children.entrySet()) {
+            int currentPosition = (int) map.getKey();
+            int currentValue = (int) map.getValue();
+            if (maxPosition == -10 || currentValue > queueArray.get(maxPosition)) {
+                maxPosition = currentPosition;
+            }
+        }
+
+        return maxPosition;
+    }
+
+    private void findBiggestChild() {
+        lookForChildren(nodePosition);
+
+        if (children.size() > 0) {
+            childPosition = maxChildPosition();
+            child = children.get(childPosition);
+            hasChild = true;
+        } else {
+            hasChild = false;
+        }
+    }
+
+    private void lookForGrandChildren(int nodePosition) {
+        grandChildren.clear();
+
+        int child1Position = nodePosition * 2 + 1;
+        int child2Position = nodePosition * 2 + 2;
+
+        int grandChild1Position = child1Position * 2 + 1;
+        int grandChild2Position = child1Position * 2 + 2;
+
+        int grandChild3Position = child2Position * 2 + 1;
+        int grandChild4Position = child2Position * 2 + 2;
+
+        addGrandChildToMap(grandChild1Position);
+        addGrandChildToMap(grandChild2Position);
+
+        addGrandChildToMap(grandChild3Position);
+        addGrandChildToMap(grandChild4Position);
+    }
+
+    private int minGrandChildPosition() {
+        int minPosition = -10;
+        for (Map.Entry map : grandChildren.entrySet()) {
+            int currentPosition = (int) map.getKey();
+            int currentValue = (int) map.getValue();
+            if (minPosition == -10 || currentValue < queueArray.get(minPosition)) {
+                minPosition = currentPosition;
+            }
+
+        }
+        return minPosition;
+    }
+
+    private void findSmallestGrandChild() {
+        lookForGrandChildren(nodePosition);
+
+        if (grandChildren.size() > 0) {
+
+            grandChildPosition = minGrandChildPosition();
+            grandChild = queueArray.get(grandChildPosition);
+
+            hasGrandChild = true;
+        } else {
+            hasGrandChild = false;
+        }
+    }
+
+    private int maxGrandChildPosition() {
+        int maxPosition = -10;
+        for (Map.Entry map : grandChildren.entrySet()) {
+            int currentPosition = (int) map.getKey();
+            int currentValue = (int) map.getValue();
+            if (maxPosition == -10 || currentValue > queueArray.get(maxPosition)) {
+                maxPosition = currentPosition;
+            }
+        }
+
+        return maxPosition;
+    }
+
+    private void findBiggestGrandChild() {
+        lookForGrandChildren(nodePosition);
+
+        if (grandChildren.size() > 0) {
+            grandChildPosition = maxGrandChildPosition();
+            grandChild = queueArray.get(grandChildPosition);
+
+            hasGrandChild = true;
+        } else {
+            hasGrandChild = false;
+        }
+    }
+
+
+    public void delete(int number) {
+        boolean numberExists = false;
+        int numberPosition = 0;
+        for (int i = 0; i < queueArray.size(); i++) {
+            if (queueArray.get(i) == number) {
+                numberPosition = i;
+                numberExists = true;
+            }
+        }
+
+        if (numberExists && numberPosition != queueArray.size() -1) {
+            swap(queueArray, numberPosition, queueArray.size() - 1);
+
+            queueArray.remove(queueArray.size() - 1);
+
+            int level = getLevel(numberPosition);
+
+            if (level == 0) {
+                setNode(numberPosition);
+                setGrandChild(nodePosition);
+                int smallestGC = grandChild;
+
+                while (hasGrandChild) {
+                    if (smallestGC < node) {
+                        swapNodeWithGrandChild();
+                    } else {
+                        break;
+                    }
+                }
+                setParent(nodePosition);
+
+                swapped = false;
+                if (node > parent) {
+                    swapNodeWithParent();
+                    swapped = true;
+                }
+
+                setChild(nodePosition);
+                if (hasChild) {
+                    if (swapped) {
+                        int biggestChild = child;
+                        if (biggestChild > node) {
+                            swapNodeWithChild();
+                        }
+                    } else {
+                        int smallestChild = child;
+                        if (smallestChild < node) {
+                            swapNodeWithChild();
+                        }
+                    }
+                }
+            } else if (level == 1) {
+                setNode(numberPosition);
+                setGrandChild(nodePosition);
+                int biggestGrandChild = grandChild;
+
+                while (hasGrandChild) {
+                    if (biggestGrandChild > node) {
+                        swapNodeWithGrandChild();
+                    } else {
+                        break;
+                    }
+                }
+                setParent(nodePosition);
+
+                swapped = false;
+                if (node < parent) {
+                    swapNodeWithParent();
+                    swapped = true;
+                }
+
+                setChild(nodePosition);
+
+                if (hasChild) {
+                    if (swapped) {
+                        int smallestChild = child;
+                        if (smallestChild < node) {
+                            swapNodeWithChild();
+                        }
+                    } else {
+                        int biggestChild = child;
+                        if (biggestChild > node) {
+                            swapNodeWithChild();
+                        }
+                    }
+                }
+            }
+        } else {
+            queueArray.remove(numberPosition);
+        }
+    }
+
+
+    public void prioritize() {
+        for (int i = 0; i < queueArray.size(); i++) {
+            int level = getLevel(i);
+
+            // Check if min (level = 0)
+            if (level == 0) {
+                // Node
+                setNode(i);
+                // Parent
+                setParent(nodePosition);
+                swapped = false;
+                if (hasParent) {
+                    if (node > parent) {
+                        // Node becomes parent (by getting parents position)
+                        swapNodeWithParent();
+                    }
+                }
+
+                setGrandParent(nodePosition);
+
+                if (swapped) {
+                    while (hasGrandParent) {
+                        if (node > grandParent) {
+                            swapNodeWithGrandParent();
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    while (hasGrandParent) {
+                        if (node < grandParent) {
+                            swapNodeWithGrandParent();
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            // Check if max (level = 1)
+            if (level == 1) {
+                // Node
+                setNode(i);
+                // Parent
+                setParent(nodePosition);
+                swapped = false;
+                if (hasParent) {
+                    if (node < parent) {
+                        // Node becomes parent (by getting parents position)
+                        swapNodeWithParent();
+                    }
+                }
+
+                // GrandParent
+                setGrandParent(nodePosition);
+                if (swapped) {
+                    while (hasGrandParent) {
+                        if (node < grandParent) {
+                            swapNodeWithGrandParent();
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    while (hasGrandParent) {
+                        if (node > grandParent) {
+                            swapNodeWithGrandParent();
+                        } else {
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    ////////////////////////////////////////////////////////////////
+    /// Helper methods for setting node / parent / grand parent /
+    /// grand children there position and there value.
+    ////////////////////////////////////////////////////////////////
+
+    private void setNode(int i) {
+        nodePosition = i;
+        node = queueArray.get(nodePosition);
+    }
+
+    private void setParent(int i) {
+        parentPosition = parentPosition(i);
+        if (parentPosition >= 0) {
+            parent = getParent(parentPosition);
+            hasParent = true;
+        } else {
+            hasParent = false;
+        }
+    }
+
+    private void setGrandParent(int i) {
+        grandParentPosition = grandParentPosition(i);
+        if (grandParentPosition >= 0) {
+            grandParent = getGrandParent(grandParentPosition);
+            hasGrandParent = true;
+        } else {
+            hasGrandParent = false;
+        }
+    }
+
+    private void setChild(int nodePosition) {
+        int level = getLevel(nodePosition);
+        if (level == 0) {
+            findSmallestChild();
+        } else if (level == 1) {
+            findBiggestChild();
+        }
+    }
+
+    private void setGrandChild(int nodePosition) {
+        int level = getLevel(nodePosition);
+
+        if (level == 0) {
+            findSmallestGrandChild();
+        } else if (level == 1) {
+            findBiggestGrandChild();
+        }
+    }
+
+    private void swapNodeWithParent() {
+        swap(queueArray, nodePosition, parentPosition);
+        setNode(parentPosition);
+
+        setParent(nodePosition);
+        swapped = true;
+    }
+
+    private void swapNodeWithGrandParent() {
+        swap(queueArray, nodePosition, grandParentPosition);
+        setNode(grandParentPosition);
+
+        setGrandParent(nodePosition);
+        swapped = true;
+    }
+
+    private void swapNodeWithChild() {
+        swap(queueArray, nodePosition, childPosition);
+        setNode(childPosition);
+
+        setChild(nodePosition);
+    }
+
+    private void swapNodeWithGrandChild() {
+        swap(queueArray, nodePosition, grandChildPosition);
+        setNode(grandChildPosition);
+
+        setGrandChild(nodePosition);
+    }
+}
+
+
